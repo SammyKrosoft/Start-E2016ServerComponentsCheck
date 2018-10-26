61f7a3b32507e4da9e36906ecdf86d45e2ed524c
Administrator <Administrator@CANADA.CA>
2018-10-26 11:02:02 -0400

    Added test for PowerShell version

--- Launch-E2016ServerComponentStateToActive.ps1
+++ Launch-E2016ServerComponentStateToActive.ps1
@@ -1,4 +1,27 @@
-﻿
+﻿Function IsPSV3 {
+    <#
+    .DESCRIPTION
+    Just printing Powershell version and returning "true" if powershell version
+    is Powershell v3 or more recent, and "false" if it's version 2.
+    .OUTPUTS
+    Returns $true or $false
+    .EXAMPLE
+    IsPSVersionV3
+    #>
+    $PowerShellMajorVersion = $PSVersionTable.PSVersion.Major
+    $msgPowershellMajorVersion = "You're running Powershell v$PowerShellMajorVersion"
+    Write-Host $msgPowershellMajorVersion -BackgroundColor blue -ForegroundColor yellow
+    If($PowerShellMajorVersion -le 2){
+        Write-Host "Sorry, PowerShell v3 or more is required. Exiting."
+        Return $false
+        Exit
+    } Else {
+        Write-Host "You have PowerShell v3 or later, great !" -BackgroundColor blue -ForegroundColor yellow
+        Return $true
+        }
+}
+
+
 Function Test-ExchTools(){
     <#
     .SYNOPSIS
@@ -283,12 +306,14 @@ Function Run-Command {
 }
 
 
+#First check for PowerShell version ... if PowerShell <v3, exit
+IsPSV3 | out-null
+#Immediately test for Exchange tools => if not loaded, exit script
+If (!(Test-ExchTools)){exit}
+
 
 # Load a WPF GUI from a XAML file build with Visual Studio
 Add-Type -AssemblyName presentationframework, presentationcore
-
-#Immediately test for Exchange tools => if not loaded, exit script
-If (!(Test-ExchTools)){exit}
 
 $wpf = @{ }
 # NOTE: Either load from a XAML file or paste the XAML file content in a "Here String"
