1602032dd6a8b3ac2abd8bc0ce487ba4e33463b2
SammyKrosoft <sammy@hotmail.fr>
2021-10-21 14:44:17 +0000

    new file ?

---
+++ newsettingsoverrideAMSI.ps1
@@ -0,0 +1,9 @@
+﻿New-SettingOverride -Name “DisablingAMSIScan” -Component Cafe -Section HttpRequestFiltering -Parameters (“Enabled=False”) -Reason “Testing”
+
+Get-ExchangeDiagnosticInfo -Process Microsoft.Exchange.Directory.TopologyService -Component VariantConfiguration -Argument Refresh
+
+Restart-Service -Name W3SVC, WAS -Force
+
+
+
+
